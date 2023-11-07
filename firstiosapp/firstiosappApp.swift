d76e174b0757173e480ee7051e71afd45534b1be
jaeho.5 <ovsoce@gmail.com>
2023-11-08 06:38:28 +0900

    Initial Commit

---
+++ firstiosapp/firstiosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstiosappApp.swift
+//  firstiosapp
+//
+//  Created by jaeho on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct firstiosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
