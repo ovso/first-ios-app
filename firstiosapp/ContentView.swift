5690c76d8c55ec299a53d52794410281368d2be4
jaeho.5 <ovsoce@gmail.com>
2023-11-09 21:47:17 +0900

    VStack

--- firstiosapp/ContentView.swift
+++ firstiosapp/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("1")
             Text("2")
             Text("3")
+                .background(.white)
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.white)
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.white)
+            Text("1")
+            Text("2")
+            Text("3")
+                .background(.white)
         }
+        .frame(width: 300, height: 200, alignment: .leading)
+        .background(.orange)
     }
 }
 
