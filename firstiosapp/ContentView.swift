d413bf35ad25241feee0c59ec2ca86af8db0a573
jaeho.5 <ovsoce@gmail.com>
2023-11-22 23:01:38 +0900

    feat: enum, array

--- firstiosapp/ContentView.swift
+++ firstiosapp/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 
 struct ContentView: View {
-
+    @State var menu: String? = "오늘 뭐먹지?"
+    let menus: [Food] = [.bulgogi, .tangsuyook, .sushi, .pho]
+    
+    enum Food: String {
+        case bulgogi = "불고기"
+        case tangsuyook = "탕수육"
+        case sushi = "스시"
+        case pho = "쌀국수"
+    }
+    
     var body: some View {
-        Image(systemName: "bolt")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 300, height: 200)
-            .background(.green)
+        VStack {
+            if menu == "불고기" || menu == "스시" {
+                Text("너무 맛있는 \(menu!)")
+            } else {
+                Text(menu!)
+            }
+            
+            Button(action: {
+                menu = menus.randomElement()?.rawValue
+            }, label: {
+                Text("눌러요!")
+            })
+        }
     }
 }
 
